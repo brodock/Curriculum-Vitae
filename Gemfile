808f8f825a40caeb0a546c2f3ec13bf44ee6ac48
Gabriel Mazetto <brodock@gmail.com>
2013-08-29 02:29:15 -0300

    Updated project and some code style fixes

--- Gemfile
+++ Gemfile
@@ -1,10 +1,4 @@
-# Ugly fix for bundler's "ArgumentError: invalid byte sequence in US-ASCII" error
-if RUBY_VERSION =~ /1.9/
-    Encoding.default_external = Encoding::UTF_8
-    Encoding.default_internal = Encoding::UTF_8
-end
-
-source :rubygems
+source 'https://rubygems.org'
 gem 'sinatra', :require => 'sinatra/base'
 gem 'sinatra-r18n', :require => 'sinatra/r18n'
 gem 'sinatra-static-assets', :require => 'sinatra/static_assets', :git => 'git://github.com/brodock/sinatra-static-assets.git'
@@ -12,5 +6,4 @@ gem 'sinatra-static-assets', :require => 'sinatra/static_assets', :git => 'git:/
 group :development do
   gem 'puma'
   gem 'capistrano'
-  gem 'rvm-capistrano'
 end
--- application.rb
+++ application.rb
@@ -3,24 +3,24 @@ class CurriculumApp < Sinatra::Base
   include Curriculum::Loader
   register Sinatra::R18n
   register Sinatra::StaticAssets
-  set :root,           File.dirname(__FILE__)
+  set :root, File.dirname(__FILE__)
   set :default_locale, 'en'
-  
+
   helpers Curriculum::Helpers
 
   get '/' do
     redirect '/en'
   end
-  
+
   get '/:locale' do
     case params[:locale]
-    when 'pt'
-      load_curriculum("curriculum.pt-BR")
-    else
-      load_curriculum("curriculum")
+      when 'pt'
+        load_curriculum('curriculum.pt-BR')
+      else
+        load_curriculum('curriculum')
     end
-      
-    erb :index, :locals => { :title => "#{@person.name} | Curriculum Vitae", :person => @person }
+
+    erb :index, :locals => {:title => "#{@person.name} | Curriculum Vitae", :person => @person}
   end
-  
+
 end
